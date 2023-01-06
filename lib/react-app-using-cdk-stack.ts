92484a7a4eb194bf8644cefaf35e025e6a2e687e
shreyansh <shreyansh9588@gmail.com>
2023-01-06 17:09:09 -0500

    add test stage

--- lib/react-app-using-cdk-stack.ts
+++ lib/react-app-using-cdk-stack.ts
@@ -11,6 +11,7 @@ import { StackProps } from 'aws-cdk-lib';
 
 export class ReactAppUsingCdkStack extends cdk.Stack {
   public readonly envUSA = { account: '157559436467', region: 'us-east-1' };
+
   constructor(scope: Construct, id: string, props: cdk.StackProps) {
     super(scope, id, props);
 
@@ -33,49 +34,11 @@ export class ReactAppUsingCdkStack extends cdk.Stack {
 
 
     // testingStage.addPre(new ShellStep("Run Unit Tests", { commands: ['npm install', 'npm test'] }));
-    testingStage.addPost(new ManualApprovalStep('Manual approval before production'));
+    // testingStage.addPost(new ManualApprovalStep('Manual approval before production'));
 
-    const prodStage = pipeline.addStage(new PipelineStage(this, "prod", {
-      env: { account: "157559436467", region: "us-east-1" }
-    }));
-
-    //1. create s3 bucket
-    // const bucket = new Bucket(this, 'ReactAppBucket', {
-    //   encryption: BucketEncryption.S3_MANAGED
-    // })
-
-    //12. role to access bucket
-
-    // const bucketAccessRole = new Role(this, `BucketAccessRole`, {
-    //   assumedBy: new ServicePrincipal("lambda.amazonaws.com")
-    // });
-
-    // const bucketAccessPolicy = new ManagedPolicy(this, "BucketAccessPolicy", {
-    //   managedPolicyName: "BucketAccessPolicy",
-    //   statements: [
-    //     new PolicyStatement({
-    //       effect: Effect.ALLOW,
-    //       actions: ['s3:GetObject', 's3:PutObject'],
-    //       resources: [
-    //         `${bucket.bucketArn}/*`
-    //       ],
-    //     }),
-    //   ]
-    // });
-
-    // bucketAccessRole.addManagedPolicy(bucketAccessPolicy);
-
-    //11. create lambda in cdk
-    //path.join work correctly on all OS - mac, windows and linux
-    // const getPhotosLambda = new Function(this, 'get-photos', {
-    //   runtime: Runtime.NODEJS_16_X,
-    //   code: Code.fromAsset(path.join(__dirname, "..", "api", "get-photos", "dist")),
-    //   handler: 'index.getPhotos',
-    //   environment: {
-    //     BUCKET_NAME: bucket.bucketName
-    //   },
-    //   role: bucketAccessRole
-    // })
+    // const prodStage = pipeline.addStage(new PipelineStage(this, "prod", {
+    //   env: { account: "157559436467", region: "us-east-1" }
+    // }));
 
   }
 }
