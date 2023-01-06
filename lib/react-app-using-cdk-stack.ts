011b888e390e00413ce4dc1928be23111065555f
shreyansh <shreyansh9588@gmail.com>
2023-01-06 11:21:23 -0500

    update source

--- lib/react-app-using-cdk-stack.ts
+++ lib/react-app-using-cdk-stack.ts
@@ -15,7 +15,7 @@ export class ReactAppUsingCdkStack extends cdk.Stack {
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'TestPipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('beabetterdevv/ci-cd-aws-pipeline-demo', 'main'), //Remember to change 
+        input: CodePipelineSource.gitHub('shreyanshj12/reactAppUsingCDK', 'main'),
         commands: ['npm ci',
           'npm run build',
           'npx cdk synth']
